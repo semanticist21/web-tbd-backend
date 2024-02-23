7f5898358303452570049b4118cea44fd37078bb
jiwon <semanticist0@gmail.com>
2024-02-23 22:52:04 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+pub mod server;
+
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(server::hello))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
