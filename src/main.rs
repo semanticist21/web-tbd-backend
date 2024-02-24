7a722c4990ea39858aa2f96268b9f0f37515cab9
jiwon <semanticist0@gmail.com>
2024-02-24 15:58:09 +0900

    🦺 cors 적용

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 pub mod server;
 
+use actix_cors::Cors;
 use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(server::hello))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    let base_addr = "127.0.0.1";
+    let port = "8080";
+
+    HttpServer::new(move || {
+        let allowed = vec!["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"];
+
+        let cors = Cors::default()
+            .allowed_origin([base_addr, port].join(":").as_str())
+            .allowed_methods(allowed);
+
+        App::new().wrap(cors).service(server::hello)
+    })
+    .bind([base_addr, port].join(":").as_str())?
+    .run()
+    .await
 }
